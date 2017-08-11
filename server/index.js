42deb13c67865ea9f111da2a42ea723ff1785087
ballonj1 <ballonj1@tcnj.edu>
2017-08-10 17:18:04 -0700

    Add a port

--- server/index.js
+++ server/index.js
@@ -5,10 +5,17 @@ app.get('/', (req, res) => {
   res.send({ hi: 'there' })
 });
 
-//app represents the underlying express server
-//app.get creates a new route handler
+// app represents the underlying express server
+// app.get creates a new route handler
 // '/' watches for an incoming HTTP request with a very specific method
 // res.send({ hi: 'there' }) -> close the request
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+// IF THERE IS NOT AN ENV VARIABLE THE PORT VARIABLE WILL BE SET TO 5000
+// Whenever heroku runs the application it has the ability to inject env variables
+// env variables are set in the underlying runtime that node is running on top of
+// things that are relvant in the environment when we are running our application
+// We won't know what our port will be ahead of time
+
+app.listen(PORT);
 // tells node that we want to listen on the port 5000
